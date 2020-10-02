5b35c29aa97882452a5845a7cb98c4cde343e1b9
Jeff Breig <jjbreig@gmail.com>
2020-10-01 21:45:55 -0400

    searching and sorting done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,17 +21,11 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-    n = len(arr) 
-    # Traverse through all array elements 
+    n = len(arr)  
     for i in range(n-1): 
-    # range(n) also work but outer loop will repeat one time more than needed. 
-  
-        # Last i elements are already in place 
+
         for j in range(0, n-i-1): 
   
-            # traverse the array from 0 to n-i-1 
-            # Swap if the element found is greater 
-            # than the next element 
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
 
